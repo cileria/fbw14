f6433a1600f733e0a9ecaddcde70034267bd3b30
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-09-06 15:04:20 +0200

    Updates

---
+++ precache-manifest.06f0f8e9b99689f3ab2d96c8c5d61df7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c88f4d02301ad47b958d4b66ce7187c",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/index.html"
+  },
+  {
+    "revision": "e5bd5bd58ad2ffe07219",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7e5377e09be058c94e83",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e5bd5bd58ad2ffe07219",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/js/2.5e3abc32.chunk.js"
+  },
+  {
+    "revision": "7e5377e09be058c94e83",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/js/main.f907b8e5.chunk.js"
+  },
+  {
+    "revision": "634d281fb56f204cf0d6",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/js/runtime~main.d3ac96a9.js"
+  },
+  {
+    "revision": "9597e25814edcd710ecde6e3e92f2e1c",
+    "url": "/foobaroo/fbw14/tree/master/react/01_helloreact/hello-react/static/media/katze.9597e258.jpg"
+  }
+]);
