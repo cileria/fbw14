086ea05db1ba44769690dc9449d76bc44d24d518
Jan Schulz <jan.schulz@devugees.org>
2019-09-17 11:47:32 +0200

    finished halloworld server

--- nodejs/09_hallowelt_server/server.js
+++ nodejs/09_hallowelt_server/server.js
@@ -1,3 +1,52 @@
+// express einbinden
+const express = require('express');
 
+// den webserver initialisieren
+const app = express();
 
+app.get('/hallo', (request, response) => {
+    return response.send('huhu wie gehts?');
+});
+
+app.get('/', (request, response) => {
+    return response.send('hallo');
+});
+
+app.get('/give_me_html', (request, response) => {
+    return response.send(`
+        <html>
+            <head></head>
+            <body>
+                <h1>Hier, das ist HTML</h1>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/jan', (request, response) => {
+    return response.send('jans website');
+});
+
+app.get('/beachbild', (request, response) => {
+    return response.send(`
+        <html>
+            <head></head>
+            <body>
+                <img src="https://img1.10bestmedia.com/Images/Photos/352450/GettyImages-913753556_55_660x440.jpg" />
+            </body>
+        </html>
+    `);
+});
+
+// Aufgabe:
+//
+// Erstelle eine weitere Route zu einer Resource
+// -> /beachbild
+//
+// 1) Gib eine simple Website zurück mit einem Head und einem Body.
+// 2) Innerhalb des Body, erstelle ein Image.
+// 3) Für das src-Attribut, suche ein Beachbild bei Google-Bildersuche,
+//    und kopiere dir dafür die Bildadresse.
+
+app.listen(3000);
 console.log('hallo welt server auf port 3000');
