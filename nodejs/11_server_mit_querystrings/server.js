858f9e1fd5128b4f9146fe92cfdb86688a50af49
Jan Schulz <jan.schulz@devugees.org>
2019-09-23 14:01:13 +0200

    finished search q

--- nodejs/11_server_mit_querystrings/server.js
+++ nodejs/11_server_mit_querystrings/server.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
 //
 // 1) Suche Dir 10 real existierende Film-Titel aus dem Internet.
 // 2) Speichere diese Filmtitel in einem Array mit 10 Elementen
+const movies = [
+    'The Shawshank Redemption',
+    'The Godfather',
+    'The Godfather: Part II',
+    'The Dark Knight',
+    '12 Angry Men',
+    'Schindlers List',
+    'The Lord of the Rings: The Return of the King',
+    'Pulp Fiction',
+    'The Good, the Bad and the Ugly',
+    'Fight Club'
+];
 // 3) Erstelle eine neue Route /search
 // 4) In der Route, werte den Wert von dem Query-Key "q" aus und
 //    gib alle Filme aus, die ähnlich wie der Wert von q sind.
@@ -26,5 +38,30 @@ app.get('/', (req, res) => {
 // Tipp: vor der dem Vergleich, konvertiere den Suchbegriff und
 //       alle Elemente im Array nach Lowercase.
 
+app.get('/search', (req, res) => {
+
+    // leeres Array, was mit den Ergebnissen aufgefüllt wird
+    const resultMovies = [];
+
+    // prüfen, ob q gegeben ist
+    if(!req.query.q) {
+        return res.send({ error: 'no q given' });
+    }
+        
+    // den suchparameter q nach lowerCase konvertieren
+    const lowerQ = new String(req.query.q).toLowerCase();
+
+    // wir durchsuchen alle movies nach nem include
+    for(movie of movies) {
+        // wir konvertieren jeden movie nach lower
+        const lowerMovie = movie.toLowerCase();
+        // wir vergleichen lowerQ mit lowerMovie
+        if(lowerMovie.includes(lowerQ)) {
+            resultMovies.push(movie);
+        }
+    }
+    
+    return res.send(resultMovies);
+});
 
 app.listen(3000);
