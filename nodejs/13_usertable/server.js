f40c390cdf7880c2d43ce999e0cd07e2847dd53c
Jan Schulz <jan.schulz@devugees.org>
2019-09-23 15:59:45 +0200

    added /users route

---
+++ nodejs/13_usertable/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+// alle dateien, die im public drin sind,
+// können von außen über den dateinamen geladen werden
+// / -> lädt automatisch index.html
+// /main.js -> lädt die datei main.js
+// /style.css -> lädt die datei style.css
+app.use('/', express.static('public'));
+
+const users = [
+    { id: 1, name: 'Stefan', email: 'stefan@abc.net' },
+    { id: 2, name: 'Lara', email: 'lara@gmx.net' },
+    { id: 3, name: 'Julia', email: 'julia@gmail.com' },
+    { id: 4, name: 'Karsten', email: 'karsten@foo.net' },
+    { id: 5, name: 'Peter', email: 'peter@xing.net' },
+]
+
+app.get('/users', (req, res) => {
+    return res.send(users);
+});
+
+// app.get('/', (req, res) => {
+//     return res.send('Hallo World');
+// });
+
+console.log('Hallo World from Backend.');
+app.listen(3000);
