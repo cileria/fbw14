fc4afbd0c7fd05e0b1d279fb6b0a169ed949a4a3
Jan Schulz <jan.schulz@digitalcareerinstitute.org>
2019-11-05 11:47:02 +0100

    progressed in signup

--- nodejs/26_signup/main.js
+++ nodejs/26_signup/main.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const express = require('express');
 
 const app = express();
-
+app.use(express.json());
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'jan',
@@ -11,30 +11,53 @@ const connection = mysql.createConnection({
     database: 'miniblog'
 });
 
-const addUser = (email, password) => {
-    // query3: daten einfügen
-    const query3 = `
-        insert into users (
-            email, 
-            password 
-        )
-        values (?,?);
-    `;
+// erweitere den signup-prozess so, 
+// 1) dass geprüft wird, ob der nutzer schon existiert.
+// falls der nutzer schon existiert, soll zurückgegeben werden:
+// { error: 1001, message: 'user already exists' }
+// 2) es soll geprüft werden, dass req.body.email und req.body.password
+// auch angegeben wurde, 
+
+const addUser = (req, res) => {
+    console.log(`incoming request, attempt to create user with email: ${req.body.email} and 
+    password: ${req.body.password}`);
+
+    if(!(req.body.email && req.body.password)) {
+        return res.send({ error: 1002, message: 'email and password required' });
+    }
+
+    const queryUserExists = `select * from users where email = ?`;
     connection.query(
-        query3, [ 
-            email,
-            password
-        ],
+        queryUserExists, [req.body.email],
         (err, rows) => {
             if(err) {
                 console.log('Error: ' + err);
                 return;
             }
 
-            console.log(rows);
-        });
+            if(rows.length > 0) {
+                return res.send({ error: 1001, message: 'user already exists' });
+            }
+
+            // user existiert noch nicht!
+            
+            const queryAddUser = `
+                insert into users (email, password) values (?, ?)
+            `;
+            connection.query(
+                queryAddUser, [req.body.email, req.body.password],
+                (err, rows) => {
+                    if(err) {
+                        console.log('Error: ' + err);
+                        return;
+                    }
+        
+                    return res.send({ error: 0, message: 'user successfully created' });
+                });    
+        });           
 }
 
+app.post('/signup', addUser);
 app.get('/users', (req, res) => {
     const query = `
         select * from users
