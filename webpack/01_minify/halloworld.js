5be03366236b0436c8f227929b0e9d96cf7d54ac
Jan Schulz <jan.schulz@digitalcareerinstitute.org>
2019-11-11 13:40:31 +0100

    started with minify to demonstrate webpack

---
+++ webpack/01_minify/halloworld.js
@@ -0,0 +1,14 @@
+function halloWorld(myName, myAddress) {
+    console.log(`
+        Hi, my name is ${myName} and my address is ${myAddress}.
+        How are you doing?
+    `)
+}
+
+class Foobar {
+    constructor(x) {
+        this.x = x;
+    }
+}
+
+halloWorld('Jan', 'Friedrichshain');
---
+++ webpack/01_minify/halloworld_minified.js
@@ -0,0 +1,4 @@
+const halloWorld=(a,b)=>{console.log(`
+        Hi, my name is ${a} and my address is ${b}.
+        How are you doing?
+    `)};halloWorld("Jan","Friedrichshain");
