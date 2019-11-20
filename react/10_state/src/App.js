a699f8fa47d3b267eb0a66fe2b0d22dc9b0268f4
Jan Schulz <jan.schulz@digitalcareerinstitute.org>
2019-11-20 11:49:59 +0100

    added state 1st example

---
+++ react/10_state/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  state = {
+    greeting1: 'Hallo World',
+    numbers: [1,2,3,4,5,6]
+  }
+  
+  constructor() {
+    super(); // ruft den constructor auf der Component-klasse
+    this.state.greeting2 = 'Hallo World from constructor';
+  }
+
+  // App hat fertig geladen
+  componentDidMount() {
+    // nach dem Fertigladen der App, 
+    // erstelle einen Timer, der nach 2 Sekunden den State updated
+    setTimeout(
+      () => {
+        this.setState({
+          greeting1: 'Changed Hallo World!',
+          x : 5
+        });
+      },
+      2000
+    )
+
+    setTimeout(
+      () => {
+        this.setState({
+          // wir erstellen eine kopie des number-arrays, fügen an die kopie
+          // eine neue zahl hinzu und speichern das neue array im neuen
+          // state
+          numbers: [...this.state.numbers, 7]
+        })
+      }
+      , 4000  
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.greeting1}
+        <br />
+        {this.state.greeting2}
+        <br />
+        x = {this.state.x}
+        <ul>
+          { this.state.numbers.map( (number) => <li>{number}</li>) }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
